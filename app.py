216c3850ebd3ac8e7a6011048588a0c61f57085d
Lucas Vasconcelos <lucas.vasconcelos@callface.com.br>
2024-05-28 20:13:52 -0300

    add deply details

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def delete_kmz(kmz_id):
         return jsonify({"error": "KMZ not found"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
