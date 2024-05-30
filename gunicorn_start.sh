4b13fb58e4f8aecbfcbf28caba0c3c4683fd1a6e
Lucas Vasconcelos <54723728+lucasmvasc@users.noreply.github.com>
2024-05-30 19:34:04 -0300

    Update gunicorn_start.sh

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/ubuntu/kmz-manager/venv/bin/activate
-exec gunicorn -w 3 -b unix:/home/ubuntu/kmz-manager/kmz_manager.sock app:app
+exec gunicorn -w 3 -b 127.0.0.1:5000 app:app
